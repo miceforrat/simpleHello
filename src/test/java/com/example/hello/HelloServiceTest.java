59728aa32325ac3b4e728081093a55fb3a694f1c
miceforrat <211250069@smail.nju.edu.cn>
2023-08-10 20:54:35 +0800

    add embedded

--- src/test/java/com/example/hello/HelloServiceTest.java
+++ src/test/java/com/example/hello/HelloServiceTest.java
@@ -3,11 +3,14 @@ package com.example.hello;
 import com.example.hello.data.HelloMessage;
 import com.example.hello.service.HelloService;
 import jakarta.annotation.Resource;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.embedded.RedisServer;
 
 
 @SpringBootTest(classes = HelloApplication.class)
@@ -15,6 +18,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 class HelloServiceTest {
     @Resource
     private HelloService helloService;
+
+    private static RedisServer redisServer;
+
+    /**
+     * 启动Redis，并在6379端口监听
+     */
+    @BeforeAll
+    static void startRedis() {
+        // https://github.com/kstyrc/embedded-redis/issues/51
+        redisServer = RedisServer.builder()
+                .port(6379)
+                .setting("maxmemory 128M") //maxheap 128M
+                .build();
+
+        redisServer.start();
+    }
+
+    /**
+     * 析构方法之后执行，停止Redis.
+     */
+    @AfterAll
+    static void stopRedis() {
+        redisServer.stop();
+    }
+
 
     @Test
     public void testMsgSetterGetter(){
