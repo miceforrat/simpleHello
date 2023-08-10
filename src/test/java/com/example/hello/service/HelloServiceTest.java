f3d07e47029e496a38aa7a6e72beb54254327ee5
miceforrat <211250069@smail.nju.edu.cn>
2023-08-10 21:25:30 +0800

    rechange try

--- src/test/java/com/example/hello/service/HelloServiceTest.java
+++ src/test/java/com/example/hello/service/HelloServiceTest.java
@@ -9,50 +9,53 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.embedded.RedisServer;
 
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootTest(classes = HelloApplication.class)
 @RunWith(SpringRunner.class)
 class HelloServiceTest {
     @Resource
     private HelloService helloService;
 
-    private static RedisServer redisServer;
-
-    /**
-     * 启动Redis，并在6379端口监听
-     */
-    @BeforeAll
-    static void startRedis() {
-        // https://github.com/kstyrc/embedded-redis/issues/51
-        redisServer = RedisServer.builder()
-                .port(6379)
-                .setting("maxmemory 128M") //maxheap 128M
-                .build();
-
-        redisServer.start();
-
-    }
-
-    /**
-     * 析构方法之后执行，停止Redis.
-     */
-    @AfterAll
-    static void stopRedis() {
-        redisServer.stop();
-    }
+//    private static RedisServer redisServer;
+//
+//    /**
+//     * 启动Redis，并在6379端口监听
+//     */
+//    @BeforeAll
+//    static void startRedis() {
+//        // https://github.com/kstyrc/embedded-redis/issues/51
+//        redisServer = RedisServer.builder()
+//                .port(6379)
+//                .setting("maxmemory 128M") //maxheap 128M
+//                .build();
+//
+//        redisServer.start();
+//
+//    }
+//
+//    /**
+//     * 析构方法之后执行，停止Redis.
+//     */
+//    @AfterAll
+//    static void stopRedis() {
+//        redisServer.stop();
+//    }
 
 
     @Test
     public void testMsgSetterGetter(){
-//        HelloMessage message = new HelloMessage();
-//        message.msg = "tester";
-//        Assertions.assertEquals(message.msg, helloService.getGreeting(message).msg);
-//
-//        Assertions.assertEquals(message.msg, helloService.getMsgShown());
+        HelloMessage message = new HelloMessage();
+        message.msg = "tester";
+        Assertions.assertEquals(message.msg, helloService.getGreeting(message).msg);
+
+        Assertions.assertEquals(message.msg, helloService.getMsgShown());
     }
 
 }
