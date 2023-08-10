60c0de09dcce1ebf7049c2428b5c38d2f5242a43
miceforrat <211250069@smail.nju.edu.cn>
2023-08-10 22:29:55 +0800

    change yaml

--- src/test/java/com/example/hello/controller/HelloControllerTest.java
+++ src/test/java/com/example/hello/controller/HelloControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@ActiveProfiles("unittest")
 class HelloControllerTest {
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/example/hello/service/HelloServiceTest.java
+++ src/test/java/com/example/hello/service/HelloServiceTest.java
@@ -2,23 +2,21 @@ package com.example.hello.service;
 
 import com.example.hello.HelloApplication;
 import com.example.hello.data.HelloMessage;
-import com.example.hello.service.HelloService;
 import jakarta.annotation.Resource;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.embedded.RedisServer;
 
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootTest(classes = HelloApplication.class)
 @RunWith(SpringRunner.class)
+@ActiveProfiles("unittest")
 class HelloServiceTest {
     @Resource
     private HelloService helloService;
@@ -28,8 +26,8 @@ class HelloServiceTest {
     /**
      * 启动Redis，并在6379端口监听
      */
-    @BeforeAll
-    static void startRedis() {
+    @BeforeClass
+    public static void startRedis() {
         // https://github.com/kstyrc/embedded-redis/issues/51
         redisServer = RedisServer.builder()
                 .port(6379)
@@ -43,8 +41,8 @@ class HelloServiceTest {
     /**
      * 析构方法之后执行，停止Redis.
      */
-    @AfterAll
-    static void stopRedis() {
+    @AfterClass
+    public static void stopRedis() {
         redisServer.stop();
     }
 
