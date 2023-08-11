ea6294d6342ef1a09a22015d9ae66b05f10ebef9
miceforrat <211250069@smail.nju.edu.cn>
2023-08-11 20:14:40 +0800

    controller tests

--- src/test/java/com/example/hello/controller/HelloControllerTest.java
+++ src/test/java/com/example/hello/controller/HelloControllerTest.java
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.transaction.annotation.Transactional;
 import redis.embedded.RedisServer;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -64,10 +62,8 @@ class HelloControllerTest {
     }
 
     @Test
-    @Transactional
-    @Rollback()
     public void doSetter() throws Exception {
-        String json = "{\"msg\":\"test\"}";
+        String json = "{\"msg\":\"testers\"}";
 //执行一个RequestBuilder请求，会自动执行SpringMVC的流程并映射到相应的控制器执行处理；
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/hello")
