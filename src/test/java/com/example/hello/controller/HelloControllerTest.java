6ee42e8c5fb942fccff8cd31d69f5356d5e3da35
miceforrat <211250069@smail.nju.edu.cn>
2023-08-11 20:10:03 +0800

    controller test

--- src/test/java/com/example/hello/controller/HelloControllerTest.java
+++ src/test/java/com/example/hello/controller/HelloControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.hello.controller;
 
 import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+import redis.embedded.RedisServer;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -23,40 +26,68 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @AutoConfigureMockMvc
 @ActiveProfiles("unittest")
 class HelloControllerTest {
-//    @Autowired
-//    private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    private static RedisServer redisServer;
+
+    /**
+     * 启动Redis，并在6379端口监听
+     */
+    @BeforeAll
+    public static void startRedis() {
+
+        System.setProperty("cur_stage", "test");
+        // https://github.com/kstyrc/embedded-redis/issues/51
+        redisServer = RedisServer.builder()
+                .port(6379)
+                .setting("maxmemory 128M") //maxheap 128M
+                .build();
+
+        redisServer.start();
+
+    }
+
+    /**
+     * 析构方法之后执行，停止Redis.
+     */
+    @AfterAll
+    public static void stopRedis() {
+        redisServer.stop();
+    }
 
     @Before
     public void setUp() throws Exception{
-//        doSetter();
-//        get();
+        doSetter();
+        get();
     }
 
-//    @Test
-//    @Transactional
-//    @Rollback()
-//    public void doSetter() throws Exception {
-//        String json = "{\"msg\":\"test\"}";
-////执行一个RequestBuilder请求，会自动执行SpringMVC的流程并映射到相应的控制器执行处理；
-//        mockMvc.perform(MockMvcRequestBuilders
-//                        .post("/hello")
-//                        .content(json.getBytes()) //传json参数
-//                        .accept(MediaType.APPLICATION_JSON)
-//                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                )
-//                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-//                .andDo(print());
-//    }
-//
-//
-//    @Test
-//    public void get() throws Exception{
-//        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
-//                .get("/hello")
-//        );
-//        resultActions.andReturn().getResponse().setCharacterEncoding("UTF-8");
-//        resultActions.andExpect(MockMvcResultMatchers.status().isOk()).andDo(print());
-//    }
+    @Test
+    @Transactional
+    @Rollback()
+    public void doSetter() throws Exception {
+        String json = "{\"msg\":\"test\"}";
+//执行一个RequestBuilder请求，会自动执行SpringMVC的流程并映射到相应的控制器执行处理；
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/hello")
+                        .content(json.getBytes()) //传json参数
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andDo(print());
+    }
+
+
+    @Test
+    public void get() throws Exception{
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .get("/hello")
+        );
+        resultActions.andReturn().getResponse().setCharacterEncoding("UTF-8");
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk()).andDo(print());
+    }
 
 }
 
