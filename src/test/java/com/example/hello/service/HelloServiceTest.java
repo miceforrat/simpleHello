39b061529daa55cf79a8545c31e434b9a51bdfc2
miceforrat <211250069@smail.nju.edu.cn>
2023-08-11 15:47:28 +0800

    to the beginning

--- src/test/java/com/example/hello/service/HelloServiceTest.java
+++ src/test/java/com/example/hello/service/HelloServiceTest.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import redis.embedded.RedisServer;
+//import redis.embedded.RedisServer;
 
 
 @SpringBootTest
