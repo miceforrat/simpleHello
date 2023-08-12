5aa2f61524f4a55fc5b00a8a20a74d93e7c78fbc
âwhy1144965912 <1144965912@qq.com>
2023-08-12 15:54:56 +0800

    1

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -1,14 +1,12 @@
 package com.example.hello;
 
-import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
-import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@EnablePrometheusEndpoint
-@EnableSpringBootMetricsCollector
+
 public class HelloApplication {
 
 	public static void main(String[] args) {
