21eb30a64bcd866eefefe6142354f689e80eb0e7
miceforrat <211250069@smail.nju.edu.cn>
2023-08-13 12:34:03 +0800

    single replica try

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -20,7 +20,7 @@ public class HelloController {
 //    private RateLimiter limiter = RateLimiter.create(100);
 
     @PostMapping("/hello")
-    @MyRedisRLimiter(name = "greet", period = 1, count = 1, key = "greet_key")
+    @MyRedisRLimiter(name = "greet", period = 1, count = 10, key = "greet_key")
     public ResponseEntity<Object> responseToGreeting(@RequestBody HelloMessage msgIn){
 
         return ResponseEntity.status(200).body(helloService.getGreeting(msgIn));
